a4907b08dd129a9497477da081bec507f45746e4
Hantong Chen <cxwdyx620@gmail.com>
2024-12-12 13:25:18 +0800

    chore: release v8.25.3+build.16957865

--- src/bilibili.app.interfaces.v1.rs
+++ src/bilibili.app.interfaces.v1.rs
@@ -464,9 +464,7 @@ pub struct CursorV2Req {
     pub player_preload: ::core::option::Option<PlayerPreloadParams>,
     ///
     #[prost(message, optional, tag = "4")]
-    pub player_args: ::core::option::Option<
-        super::super::archive::middleware::v1::PlayerArgs,
-    >,
+    pub player_args: ::core::option::Option<super::super::card::v1::PlayerArgs>,
     ///
     #[prost(bool, tag = "5")]
     pub is_local: bool,
@@ -910,7 +908,9 @@ pub struct MediaVideoReq {
     pub ps: i32,
     ///
     #[prost(message, optional, tag = "7")]
-    pub player_args: ::core::option::Option<super::super::card::v1::PlayerArgs>,
+    pub player_args: ::core::option::Option<
+        super::super::archive::middleware::v1::PlayerArgs,
+    >,
 }
 ///
 #[derive(Clone, PartialEq, ::prost::Message)]
@@ -1189,7 +1189,9 @@ pub struct SearchArchiveReq {
     pub ps: i64,
     ///
     #[prost(message, optional, tag = "5")]
-    pub player_args: ::core::option::Option<super::super::card::v1::PlayerArgs>,
+    pub player_args: ::core::option::Option<
+        super::super::archive::middleware::v1::PlayerArgs,
+    >,
 }
 ///
 #[derive(Clone, PartialEq, ::prost::Message)]
@@ -1218,7 +1220,9 @@ pub struct SearchDynamicReq {
     pub ps: i64,
     ///
     #[prost(message, optional, tag = "5")]
-    pub player_args: ::core::option::Option<super::super::card::v1::PlayerArgs>,
+    pub player_args: ::core::option::Option<
+        super::super::archive::middleware::v1::PlayerArgs,
+    >,
 }
 ///
 #[derive(Clone, PartialEq, ::prost::Message)]
